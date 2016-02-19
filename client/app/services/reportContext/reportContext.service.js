97f8376deb08e35403a3c8322a1ad8893d3ac98d
Jeronimo Vallelunga <j.vallelunga@globant.com>
2016-02-19 14:45:45 -0300

    Report Context Service

--- client/app/services/reportContext/reportContext.service.js
+++ client/app/services/reportContext/reportContext.service.js
@@ -1,6 +1,37 @@
 let reportContextService = function () {
   "ngInject";
+  
+  let datasource = null;
+  let variables = [];
+  
+  let create = () => {
+    datasource = null;
+    variables = [];    
+  }
+  
+  let getDataSource = () => datasource;
+  let setDataSource = value => datasource = value;
+  
+  let pushVariable = value => variables.push(value);
+  let pullVariable = value => { 
+    let index = variables.indexOf(value);
+    if (index > -1) { 
+      variables.splice(index, 1);
+    }
+  }
+  let getVariables = () => variables;
+  
   return {
+    create,
+    datasource: {
+      get: getDataSource,
+      set: setDataSource
+    },
+    variables: {
+      push: pushVariable,
+      pull: pullVariable,
+      get: getVariables
+    }
   };
 };
 
