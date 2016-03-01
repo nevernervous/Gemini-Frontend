2af6193d5ca22091fa746a61b01b0b9d54620632
nevernervous <brndncostoya@gmail.com>
2016-03-01 13:35:13 -0600

    Fixes with styles in many resolutions

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.js
@@ -5,10 +5,10 @@ import ualDataSourceGroup from './ualDataSourceGroup/ualDataSourceGroup';
 import ualDataSourceLabel from './ualDataSourceLabel/ualDataSourceLabel';
 
 let ualDataSourceModule = angular.module('ualDataSource', [
-  ualDataSourceChangeModal.name,
-  ualDataSourceGroup.name,
-  ualDataSourceLabel.name
-])
-.factory('ualDataSource', ualDataSourceFactory);
+        ualDataSourceChangeModal.name,
+        ualDataSourceGroup.name,
+        ualDataSourceLabel.name
+    ])
+    .factory('ualDataSource', ualDataSourceFactory);
 
 export default ualDataSourceModule;
