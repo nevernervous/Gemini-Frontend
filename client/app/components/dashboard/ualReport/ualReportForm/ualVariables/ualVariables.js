ae89f3ac55b65d5a08e0a2588575976a1cb6be43
Jeronimo Vallelunga <j.vallelunga@globant.com>
2016-03-02 10:43:44 -0300

    Task[19530]:VariablesCancelModal

--- client/app/components/dashboard/ualReport/ualReportForm/ualVariables/ualVariables.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualVariables/ualVariables.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 import ualVariablesFactory from './ualVariables.factory';
+import ualVariablesCancelModal from './ualVariablesCancelModal/ualVariablesCancelModal';
 import ualVariablesDeteleAllModal from './ualVariablesDeteleAllModal/ualVariablesDeteleAllModal';
 import ualVariableSelectedItem from './ualVariableSelectedItem/ualVariableSelectedItem';
 
 let ualVariablesModule = angular.module('ualVariables', [
+  ualVariablesCancelModal.name,
   ualVariablesDeteleAllModal.name,
   ualVariableSelectedItem.name
 ])
