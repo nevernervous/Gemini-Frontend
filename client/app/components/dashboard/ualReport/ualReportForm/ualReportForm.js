462803a0530d36722eee1038ed02ead02751c79e
nevernervous <brndncostoya@gmail.com>
2016-03-02 15:18:57 -0600

    Fixes, the app was crashing

--- client/app/components/dashboard/ualReport/ualReportForm/ualReportForm.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualReportForm.js
@@ -3,14 +3,12 @@ import uiRouter from 'angular-ui-router';
 import ualDataSource from './ualDataSource/ualDataSource';
 import ualVariables from './ualVariables/ualVariables';
 import ualReportFormComponent from './ualReportForm.component';
-import ualSearchFilter from './ualSearchFilter/ualSearchFilter';
 
 let ualReportFormModule = angular.module('ualReportForm', [
   uiRouter,
   ualVariables.name,
   // MODALS
-  ualDataSource.name,
-  ualSearchFilter.name
+  ualDataSource.name
 ])
 
 .config(($stateProvider, $urlRouterProvider) => {
