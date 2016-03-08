4c2174d368eacc0056787e3f9c4245d9cd0daea4
nevernervous <brndncostoya@gmail.com>
2016-03-08 10:08:59 -0600

    Ajuste scon scroll en datasources

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
@@ -1,47 +1,51 @@
 class UalDataSourceController {
-  /*@ngInject*/
-  constructor(close, DataSource, selected, ualDataSourceChangeModal, $filter) {
-    this._close = close;
-    this._datasource = DataSource;
-    this._changemodal = ualDataSourceChangeModal;
-    this._selected = selected;
-    this._filter = $filter;
-    this.searchTerm = {};
+    /*@ngInject*/
+    constructor(close, DataSource, selected, ualDataSourceChangeModal, $filter) {
+        this._close = close;
+        this._datasource = DataSource;
+        this._changemodal = ualDataSourceChangeModal;
+        this._selected = selected;
+        this._filter = $filter;
+        this.searchTerm = {};
 
-    this.datasources;
-    this.selected = selected;
-    this._initialize();
-  }
-
-  apply() {
-    if (this._selected && (this._selected.id !== this.selected.id) ) {
-      this._changemodal.open({ oldDataSource: this._selected, newDataSource: this.selected })
-        .then(response => response && this._close(this.selected));
-    } else {
-      this._close(this.selected);
+        this.datasources;
+        this.selected = selected;
+        this._initialize();
     }
 
-  }
-  cancel() {
-    this._close(this._selected);
-  }
+    apply() {
+        if (this._selected && (this._selected.id !== this.selected.id)) {
+            this._changemodal.open({ oldDataSource: this._selected, newDataSource: this.selected })
+                .then(response => response && this._close(this.selected));
+        } else {
+            this._close(this.selected);
+        }
 
-  shouldShow(group) {
-    return this._filter("filter")(group.items, this.searchTerm).length > 0;
-  }
+    }
+    cancel() {
+        this._close(this._selected);
+    }
 
-  isActive(itemId) {
-    return (this.selected) && this.selected.id === itemId;
-  }
+    shouldShow(group) {
+        return this._filter("filter")(group.items, this.searchTerm).length > 0;
+    }
 
-  toogleSelected(item) {
-    this.selected = this.isActive(item.id) ? null : item;
-  }
+    hasManyColumns() {
+           
+    }
 
-  _initialize() {
-    this._datasource.all('group')
-      .then(response => this.datasources = response.data);
-  }
+    isActive(itemId) {
+        return (this.selected) && this.selected.id === itemId;
+    }
+
+    toogleSelected(item) {
+        this.selected = this.isActive(item.id) ? null : item;
+    }
+
+    _initialize() {
+        this._datasource.all('group')
+            .then(response => this.datasources = response.data);
+    }
 }
 
 export default UalDataSourceController;
