14a215470f2f79370009a035dd94612cc8c9c562
Jeronimo Vallelunga <jeronimo.vallelunga@gmail.com>
2016-03-10 17:03:23 +0000

    Bug 21287: Restore UserId from session

--- client/app/components/login/loginForm/loginForm.controller.js
+++ client/app/components/login/loginForm/loginForm.controller.js
@@ -7,17 +7,19 @@ class LoginFormController {
     this._state = $state;
     this.loading = false;
     this.error = false;
+
+    this.user = Session.get() ? Session.get().username : null;
   }
-  
+
   submit(form) {
-    this.error = false; 
+    this.error = false;
     this.loading = true;
     this._session.login(this.user, this.pass)
     .then ( () => this._state.go('dashboard.report-list'))
     .catch( response => this.error = response.data )
     .finally(() => {
       this.loading = false;
-    });      
+    });
   }
 }
 
