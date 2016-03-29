0854198588f85db720df2e3f312ee60224d9bd76
nevernervous <brndncostoya@gmail.com>
2016-03-29 12:20:06 -0600

    Fix with hardcoded colors

--- client/app/app.constants.js
+++ client/app/app.constants.js
@@ -2,7 +2,7 @@
 module.exports = angular.module("app.constants", [])
 
 .constant("Properties", {
-	"endpoint": "http://vm.ardbookpror.mac:8889/api",
+	"endpoint": "http://10.230.7.8:8889/api",
 	"cache": false,
 	"fallback": {
 		"endpoint": "http://private-f47c4-ualgemini.apiary-mock.com/api"
