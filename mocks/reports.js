70e477648499cd383b630aed914a75b503bb423d
irixa <irisaortegad@gmail.com>
2016-04-01 09:11:01 -0600

    Create Report service

---
+++ mocks/reports.js
@@ -0,0 +1,191 @@
+module.exports = {
+  '/api/Reports': {
+    GET: {
+      data: {
+        "data": [
+          {
+            "id": 1,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 2,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 3,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 4,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 5,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 6,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 7,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 8,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 9,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 10,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+          {
+            "id": 11,
+            "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+          },
+              {
+                "id": 12,
+                "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+              },
+              {
+                "id": 13,
+                "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+              },
+                  {
+                    "id": 14,
+                    "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+                  },
+              {
+                "id": 15,
+               "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+              },
+                  {
+                    "id": 16,
+                   "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+                  },
+              {
+                "id": 17,
+                "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+              },
+                  {
+                    "id": 18,
+                    "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+                  },
+              {
+                "id": 19,
+                "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+              },
+                  {
+                    "id": 20,
+                    "name": "My First Report",
+            "dataSource": {
+              "dataSourceId": 1,
+              "dataSourceName": "Data Source Name"
+            },   
+            "lastModificationDate": 2
+                  }
+        ]
+      },
+      code: 200
+    }
+  }
+};
