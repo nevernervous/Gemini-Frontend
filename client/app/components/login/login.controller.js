831bf4771558d4d55e920f64027c5c550d031036
salvador.elvira <salvador_elvira@outlook.com>
2016-04-04 11:47:09 -0500

    Task [23426]: Applying the common message banner in the login screen

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -3,6 +3,7 @@ class LoginController {
   constructor($location, Token) {
     this.name = 'login';
     this.expired = Token.wasExpired();
+    this.loginMessage = {type: "",icon:"icon-warning",text:"Due to inactivity your session has expired"};
   }
 }
 
