627edc76af9d8e98bea5e7cc98744f7b58ca905e
nevernervous <brndncostoya@gmail.com>
2016-04-05 13:23:47 -0500

    Fix with ualDropDown on expirationmodal

--- client/app/common/ualModal/ualModal.factory.js
+++ client/app/common/ualModal/ualModal.factory.js
@@ -1,54 +1,58 @@
-let ualModalService = function($q, $rootScope, ModalService) {
-    "ngInject";
-    let _modal;
+let ualModalService = function($q, $rootScope, ModalService, $timeout) {
+  "ngInject";
+  let _modal;
 
-    let open = (options) => {
-        let deferred = $q.defer();
+  let open = (options) => {
+    let deferred = $q.defer();
 
-        ModalService.showModal(options)
-            .then(modal => {
-                $("ual-modal button").focus();
-                setBackwardNavigation(true);
-                _modal = modal;
-                modal.close
-                    .then(result => deferred.resolve(result))
-                    .finally(() => {
-                        setBackwardNavigation(false);
-                        _modal.controller.onClose && _modal.controller.onClose()
-                    });
-            })
-            .catch(error => deferred.reject(error));
-
-        return deferred.promise;
-    }
-
-    let close = () => {
-        _modal.controller._close();
-    }
-
-    let preventBackwardNavigation = (event) => {
-        event = event || window.event;
-        let target = event.target;
-        let code = event.keyCode || event.which;
-        if (code === 8 && !(target.tagName === "INPUT" || target.tagName === "TEXTAREA")) {
-            event.preventDefault();
-        }
-    };
-    
-
-    let setBackwardNavigation = (enable) => {
-        var $body = angular.element(document.getElementsByTagName("body"));
+    ModalService.showModal(options)
+      .then(modal => {
+        $timeout(() => {
+          $('body').click();
+        })
         
-        if (enable) {
-            $body.bind('keydown', preventBackwardNavigation);
-        } else {
-            $body.unbind('keydown', preventBackwardNavigation);
-        }
+        $("ual-modal button").focus();
+        setBackwardNavigation(true);
+        _modal = modal;
+        modal.close
+          .then(result => deferred.resolve(result))
+          .finally(() => {
+            setBackwardNavigation(false);
+            _modal.controller.onClose && _modal.controller.onClose()
+          });
+      })
+      .catch(error => deferred.reject(error));
 
+    return deferred.promise;
+  }
+
+  let close = () => {
+    _modal.controller._close();
+  }
+
+  let preventBackwardNavigation = (event) => {
+    event = event || window.event;
+    let target = event.target;
+    let code = event.keyCode || event.which;
+    if (code === 8 && !(target.tagName === "INPUT" || target.tagName === "TEXTAREA")) {
+      event.preventDefault();
+    }
+  };
+
+
+  let setBackwardNavigation = (enable) => {
+    var $body = angular.element(document.getElementsByTagName("body"));
+
+    if (enable) {
+      $body.bind('keydown', preventBackwardNavigation);
+    } else {
+      $body.unbind('keydown', preventBackwardNavigation);
     }
 
+  }
 
-    return { open, close };
+
+  return { open, close };
 };
 
 export default ualModalService;
