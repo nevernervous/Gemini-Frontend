a6d28b8fd38101ab5fd408d3f2ddcec4aa45bfe4
irixa <irisaortegad@gmail.com>
2016-04-06 11:43:21 -0500

    Task 23468:DEV - FE | Delete Report (50%). WE need to add a Delete service

---
+++ client/app/components/dashboard/ualReport/ualReportList/ualReportListDeleteReportModal/ualReportListDeleteReportModal.controller.js
@@ -0,0 +1,25 @@
+class UalReportListDeleteReportModalController {
+  /*@ngInject*/
+  constructor($rootScope, close) {
+    this.name = 'ualVariablesDeteleAllModal';
+    this._close = close;
+
+    this._suscriptions = [];
+    this._suscriptions.push($rootScope.$on('SESSION.LOGOUT', () =>  this._closemodal(true)));
+    this._suscriptions.push($rootScope.$on('SESSION.EXPIRED', () => this._closemodal(true)));
+  }
+
+  _closemodal(response) {
+    this._suscriptions.forEach(suscription => suscription());
+    this._close(response);
+  }
+
+  cancel() {
+    this._closemodal(false);
+  }
+  delete() {
+    this._closemodal(true);
+  }
+}
+
+export default UalReportListDeleteReportModalController;
