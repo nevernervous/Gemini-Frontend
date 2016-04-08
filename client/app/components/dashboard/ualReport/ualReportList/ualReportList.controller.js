8b8936727ea06eff8c54df206a1cea27df39bc4d
irixa <irisaortegad@gmail.com>
2016-04-08 18:20:33 -0500

    Task 23464:DEV - FE | Sort list

--- client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
+++ client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
@@ -1,54 +1,29 @@
 class UalReportListController {
   /*@ngInject*/
-  constructor(Report) {
-    this.name = 'ualReportList';
-        
-    this.reports = [];
-    
+  constructor(Report, $filter) {
+    this.name = 'ualReportList';        
+    this.reports = [];    
     this._reportService = Report;
+    this._filter = $filter;
+    this.predicate = 'lastModificationDate';
+    this.reverse = true;
     
-    this._initialize();    
+    this._initialize();
   }
   
   _initialize() {
-
     this._reportService.all()
       .then(response => this.reports = _.chain(response.data)
                                        .sortByOrder(['lastModificationDate'], ['desc'])
-                                       .value());                                          
-  }
+                                       .value());                                           
+  }  
   
-  sortColumn(e, args) {  
-    let icon = $("#"+e.target.id).find("i");          
-    let columns = [];
-    let order = [];
-    let isAscending = false;
-    
-    if ($(".-show-sort").parent().parent()[0].id != icon.parent().parent()[0].id) 
-        $(".-show-sort").removeClass("ion-ios-arrow-up").removeClass("ion-ios-arrow-down");       
-    
-    $(".-show-sort").removeClass("-show-sort").addClass("-hide-sort"); 
-    icon.removeClass("-hide-sort").addClass("-show-sort");
-     
-    isAscending = !icon.hasClass( "ion-ios-arrow-up" );
-      
-    icon.hasClass( "ion-ios-arrow-up" ) ? icon.removeClass("ion-ios-arrow-up").addClass("ion-ios-arrow-down")
-                                        : icon.removeClass("ion-ios-arrow-down").addClass("ion-ios-arrow-up");
-      
-    $.each(args, function() {
-      $.each(this, function(key, value) {
-        key == "column" ? columns.push(value) : order.push(isAscending ? 'asc' : 'desc');
-      });      
-    })
-      
-    if (e.target.id == "ual-column-data-source"){
-      order[order.length - 1] = 'asc';
-    }
-      
-    this.reports = _.chain(this.reports)
-                    .sortByOrder(columns, order)
-                    .value();                  
-  }
+  order(predicate) {
+    console.log(predicate);  
+    this.predicate = predicate;
+    this.reverse = (this.predicate === predicate) ? !this.reverse : false;
+    this.reports = this._filter('orderBy')(this.reports, predicate, this.reverse);
+  };  
   
 }
 
