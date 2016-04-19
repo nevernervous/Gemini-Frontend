0c6e92639b2e58af2dc772e3aad2cecb39c4e776
irixa <irisaortegad@gmail.com>
2016-04-19 18:23:43 -0500

    Refactor tooltip (95%)

--- client/app/common/ualTooltip/ualTooltip.directive.js
+++ client/app/common/ualTooltip/ualTooltip.directive.js
@@ -21,31 +21,31 @@ class ualTooltipDirective {
        ualTooltipShow => {
          let action = ualTooltipShow ? 'addClass' : 'removeClass';
 
+         let tooltip = $("#"+elem.attr("id"));
+         let container = $("#" + scope.ualTooltipContainer);
+         let offset = container.offset();
+
          if (ualTooltipShow) {
            //Cálculo de la posición
-           let tooltip = $("#"+elem.attr("id"));
-           let container = $("#" + scope.ualTooltipContainer);
-           let offset = container.offset();
-           let parent = container.closest("ual-data-source-item");
-
-           let bigSum = (offset.left + container.width() + (tooltip.width() + 25)); //Real tooltip width = 20+5 padding, right/left
-           let isLeftTooltip = (bigSum > (window.innerWidth - 15)); //15 Scroll
+           let isLeftTooltip = (offset.left + container.outerWidth(true) + tooltip.outerWidth(true)) > window.innerWidth; //TODO: Have Scroll? + 15
 
            if (isLeftTooltip){
-             console.log(scope.ualTooltipContainer, offset.left, container.width(), container.outerWidth(), container.outerWidth(true), tooltip.width(), (tooltip.width() + 25), tooltip.outerWidth(), tooltip.outerWidth(true), bigSum, window.innerWidth);
-             offset.left = offset.left - (tooltip.width() + 25);
+             offset.left = offset.left - tooltip.outerWidth(true);
              elem[action]('-tooltip-left');
            } else {
-             offset.left = container.width() + parseInt(scope.ualTooltipOptions.left) + offset.left;
+             offset.left =  offset.left + container.outerWidth(true) + parseInt(scope.ualTooltipOptions.left);
              elem[action]('-tooltip-right');
            }
 
            offset.top -= ((tooltip.height() / 2) - ((container.height() / 2)));
            offset.top += parseInt(scope.ualTooltipOptions.top) ? parseInt(scope.ualTooltipOptions.top) : 0;
-           tooltip.css(offset);
-
+         }
+         else {
+           offset.left = 0;
+           offset.top = 0;
          }
 
+         tooltip.css(offset);
          elem[action]('-show-tooltip');
 
        });
