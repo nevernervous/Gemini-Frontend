609f76d1380d9a70e5d97c36f18d6896102f4042
irixa <irisaortegad@gmail.com>
2016-04-22 11:25:28 -0500

    Remove show from data source list

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
@@ -112,25 +112,6 @@ class UalDataSourceController {
     //     }
     // }
 
-    showTooltip(e){
-      let datasource = $("#"+e.target.id);
-
-      if (e.target.nodeName == 'UAL-DATA-SOURCE-ITEM') {
-        let span = datasource.find("span:eq(0)");
-        let offset = span.offset();
-        let parentWidth = span.width();
-        let childWidth = datasource.find("span:eq(1)").width();
-        let tooltip = datasource.find("ual-tooltip");
-
-        offset.left = (childWidth > parentWidth ?  (parentWidth + 5) : (childWidth + 10)) + offset.left;
-        offset.top -= ((tooltip.height() / 2) - ((span.height() / 2) ) );
-        offset.top += window.isIE  ? 2 : 5;
-
-        tooltip.removeClass("-hide-tooltip").addClass("-show-tooltip");
-        tooltip.css(offset);
-      }
-    }
-
     hideTooltip(){
       $(".-tooltip").removeClass("-show-tooltip");
       $("[ual-tooltip-show]").prop("ual-tooltip-show", false);
