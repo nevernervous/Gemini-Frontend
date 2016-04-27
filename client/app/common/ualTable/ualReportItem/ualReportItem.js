fab0632c5b7b778633c2cd6f39ddafb5a86166d5
Iris Ortega <Iris Ortega>
2016-04-27 14:07:32 -0500

    Report row item html

--- client/app/common/ualTable/ualReportItem/ualReportItem.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import ualReportItemComponent from './ualReportItem.component';
-
-let ualReportItemModule = angular.module('ualReportItem', [])
-
-.component('ualReportItem', ualReportItemComponent);
-
-export default ualReportItemModule;
--- client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
+++ client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
@@ -1,18 +1,18 @@
 class UalReportListController {
   /*@ngInject*/
 
-  constructor(Report) {       
-    this.reports = null;    
+  constructor(Report) {
+    this.reports = null;
     this._reportService = Report;
-              
+
     this.predicate = 'lastModificationDate';
     this.reverse = true;
   }
-  
+
   $onInit() {
     this._reportService.all()
-      .then(response => this.reports = response.data);                                           
-  }  
+      .then(response => this.reports = response.data);
+  }
 }
 
 export default UalReportListController;
