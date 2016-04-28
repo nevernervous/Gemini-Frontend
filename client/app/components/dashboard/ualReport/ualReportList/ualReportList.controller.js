02afe2c76d8c99e6912fac86d86c99c0ceec7df3
irixa <irisaortegad@gmail.com>
2016-04-28 13:16:34 -0500

    Commit,

--- client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
+++ client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
@@ -13,6 +13,20 @@ class UalReportListController {
     this._reportService.all()
       .then(response => this.reports = response.data);
   }
+
+  showTooltip(e){
+    if (e){
+      if (e.target.id != ""){
+        console.log(e.target.nodeName);
+      }
+      //let tooltip = $("#"+e.target.id).first().next();
+      //tooltip.prop("ual-tooltip-show", true);
+    }
+  }
+  hideTooltip(){
+    $(".-tooltip").removeClass("-show-tooltip");
+    $("[ual-tooltip-show]").prop("ual-tooltip-show", false);
+  }
 }
 
 export default UalReportListController;
