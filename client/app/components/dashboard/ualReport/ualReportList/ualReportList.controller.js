d3bf688aced7c9c0c7cd4625c1539528ebc8f6ad
irixa <irisaortegad@gmail.com>
2016-04-28 18:40:48 -0500

    Task 26098 : DEV - FE | Report list item component (Listado con tooltips)

--- client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
+++ client/app/components/dashboard/ualReport/ualReportList/ualReportList.controller.js
@@ -14,18 +14,57 @@ class UalReportListController {
       .then(response => this.reports = response.data);
   }
 
-  showTooltip(e){
-    if (e){
-      if (e.target.id != ""){
-        console.log(e.target.nodeName);
-      }
-      //let tooltip = $("#"+e.target.id).first().next();
-      //tooltip.prop("ual-tooltip-show", true);
+  showTooltipName(id){
+    if (this.reportNameHasEllipsis(id)) {
+      let tooltip = $("#tooltipName_"+id);
+      tooltip.prop("ual-tooltip-show", true);
     }
   }
+
+  showTooltipDataSource(id){
+    let tooltip = $("#tooltipDataSource_"+id);
+    tooltip.prop("ual-tooltip-show", true);
+  }
+
   hideTooltip(){
     $(".-tooltip").removeClass("-show-tooltip");
     $("[ual-tooltip-show]").prop("ual-tooltip-show", false);
+  }
+
+  reportNameHasEllipsis(id){
+    let container = $("#reportNameContainer_" + id);
+    let sibling = $("#reportNameItem_" + id);
+    return (window.isIE) ? ((sibling.outerWidth(true)+45) >= container.width()) : (sibling.width() > container.width());
+  }
+
+  reportNameContainer(id){
+    return (this.reportNameHasEllipsis(id)) ? "reportNameContainer_" + id : "reportNameItem_" + id;
+  }
+
+  reportNameOffsetRight(id){
+    return (this.reportNameHasEllipsis(id)) ? (window.isIE ? 10 : 4) : 15;
+  }
+
+  reportNameOffsetTop(id){
+    return (this.reportNameHasEllipsis(id)) ? 0 : -3;
+  }
+
+  reportDataSourceHasEllipsis(id){
+    let container = $("#reportDataSourceContainer_" + id);
+    let sibling = $("#reportDataSourceItem_" + id);
+    return (window.isIE) ? ((sibling.outerWidth(true)+45) >= container.width()) : (sibling.width() > container.width());
+  }
+
+  reportDataSourceContainer(id){
+    return (this.reportDataSourceHasEllipsis(id)) ? "reportDataSourceContainer_" + id : "reportDataSourceItem_" + id;
+  }
+
+  reportDataSourceOffsetRight(id){
+    return (this.reportDataSourceHasEllipsis(id)) ? (window.isIE ? 10 : 4) : 15;
+  }
+
+  reportDataSourceOffsetTop(id){
+    return (this.reportDataSourceHasEllipsis(id)) ? 1 : -3;
   }
 }
 
