397c76bcff2f270160f3ffae846d0e4cf4a7289d
salvador.elvira <salvador_elvira@outlook.com>
2016-04-28 21:12:34 -0500

    Changing unsafe modal from carpet

--- client/app/components/dashboard/ualUnsafeReportModal/ualUnsafeReportModal.js
+++ client/app/components/dashboard/ualUnsafeReportModal/ualUnsafeReportModal.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import ReportService from '../../../../../services/report/report';
+import ReportService from '../../../services/report/report';
 
 import ualUnsafeReportModalFactory from './ualUnsafeReportModal.factory';
 
