e6fbc2adb1d488eac0aecd690464dbda04f75bcd
nevernervous <brndncostoya@gmail.com>
2016-04-29 13:36:32 -0500

    Adjustments with layout

--- client/app/app.constants.js
+++ client/app/app.constants.js
@@ -5,7 +5,7 @@ module.exports = angular.module("app.constants", [])
 	"endpoint": "http://localhost:8889/api",
 	"cache": false,
 	"fallback": {
-		"endpoint": "http://localhost:8889/api"
+		"endpoint": "http://private-f47c4-ualgemini.apiary-mock.com/api"
 	}
 })
 
