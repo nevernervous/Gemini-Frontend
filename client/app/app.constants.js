653b26a29272d608cd1d161ad5c0ba4b0abac863
nevernervous <brndncostoya@gmail.com>
2016-04-29 12:53:30 -0500

    Fix with message banner

--- client/app/app.constants.js
+++ client/app/app.constants.js
@@ -5,7 +5,7 @@ module.exports = angular.module("app.constants", [])
 	"endpoint": "http://vcld16gdgemap01.global.ual.com:8080/api",
 	"cache": false,
 	"fallback": {
-		"endpoint": "http://private-f47c4-ualgemini.apiary-mock.com/api"
+		"endpoint": "http://localhost:8889/api"
 	}
 })
 
