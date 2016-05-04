b31c4615947e418818bd9a86e127bcd0f759da1d
gzepol2 <gerardo.lopez@globant.com>
2016-05-04 13:08:10 -0500

    add swap on positions of tooltip and type parameter

--- client/app/common/ualTooltip/ualTooltip.service.js
+++ client/app/common/ualTooltip/ualTooltip.service.js
@@ -3,11 +3,17 @@ import './ualTooltipService.scss';
 let ualTooltipService = function () {
   "ngInject";
 
-
   let show = (options) => {
     let tooltip=$("#tooltip");
     let container = $("#" +options.container);
     let offset = container.offset();
+    let hasEllipsis=$("#" +options.container).has("span").length;
+    let adjust = {
+      left: hasEllipsis ? 2 : -4,
+      right: hasEllipsis ? (window.isIE ? 3 : 4) : (window.isIE ? 10 : 15),
+      top: window.isIE ? 2 : 5,
+      bottom:  window.isIE ? 2 : 5
+    }
 
     //set Text
     $("#tooltip > .container").html(options.text);
@@ -16,44 +22,61 @@ let ualTooltipService = function () {
     tooltip.removeClass('-tooltip-left');
     tooltip.removeClass('-tooltip-top');
     tooltip.removeClass('-tooltip-bottom');
+    tooltip.removeClass('-error');
 
-    //get of position
-
-    switch (options.position) {
+    switch(options.position) {
       case "left":
-        offset.left = offset.left + (parseInt(options.adjust.left) ? parseInt(options.adjust.left) : 0) - tooltip.outerWidth(true);
-        tooltip.addClass('-tooltip-left');
-        offset.top -= ((tooltip.height() / 2) - ((container.height() / 2)));
-        offset.top += parseInt(options.adjust.top) ? parseInt(options.adjust.top) : 0;
+        options.position = ( offset.left - tooltip.outerWidth(true) ) < 0 ? "right" : "left";
         break;
       case "right":
-        offset.left =  offset.left + container.outerWidth(true) + (parseInt(options.adjust.right) ? parseInt(options.adjust.right) : 0);
-        tooltip.addClass('-tooltip-right');
-        offset.top -= ((tooltip.height() / 2) - ((container.height() / 2)));
-        offset.top += parseInt(options.adjust.top) ? parseInt(options.adjust.top) : 0;
+        options.position = ( offset.left + container.outerWidth(true) + tooltip.outerWidth(true) ) > window.innerWidth ? "left" : "right";
         break;
       case "top":
-        offset.top =  offset.top - $("#tooltip").outerHeight(true);
-        tooltip.addClass('-tooltip-top');
-        offset.left -= ((tooltip.outerWidth() / 2) - ((container.width() / 2)));
-        offset.left += parseInt(options.adjust.top) ? parseInt(options.adjust.top) : 0;
+        options.position = ( offset.top - tooltip.outerHeight(true) ) > 0 ? "top" : "bottom";
         break;
       case "bottom":
-        offset.top =  offset.top + container.outerHeight(true);
-        tooltip.addClass('-tooltip-bottom');
-        offset.left -= ((tooltip.outerWidth() / 2) - ((container.width() / 2)));
-        offset.left += parseInt(options.adjust.top) ? parseInt(options.adjust.top) : 0;
+        options.position = ( offset.top + container.outerHeight(true) + tooltip.outerHeight(true) ) > window.innerHeight ? "top" : "bottom";
         break;
-      default:
+    }
+
+    //get of position
+    switch (options.position) {
+      case "left":
+        offset.left = offset.left + adjust.left - tooltip.outerWidth(true);
+        tooltip.addClass('-tooltip-left');
+        offset.top -= ( (tooltip.height() / 2) - (container.height() / 2) );
+        offset.top += adjust.top;
+        break;
+      case "right":
+        offset.left =  offset.left + adjust.right + container.outerWidth(true) ;
+        tooltip.addClass('-tooltip-right');
+        offset.top -= ( (tooltip.height() / 2) - (container.height() / 2) );
+        offset.top += adjust.top;
+        break;
+      case "top":
+        offset.top =  offset.top - tooltip.outerHeight(true);
+        tooltip.addClass('-tooltip-top');
+        offset.left -= ( (tooltip.outerWidth() / 2) - (container.width() / 2) );
+        offset.left += adjust.left
+        break;
+      case "bottom":
+        offset.top =  offset.top + adjust.bottom + container.outerHeight(true);
+        tooltip.addClass('-tooltip-bottom');
+        offset.left -= ( (tooltip.outerWidth() / 2) - (container.width() / 2) );
+        offset.left += adjust.left;
+        break;
      }
 
     tooltip.css(offset);
-    //DEBUG:
-    tooltip.css(
-      {'display':'block'});
+
+    if(options.type){
+      tooltip.addClass(options.type);
+    }
+
+    //SHOW:
+    tooltip.css('display','block');
 
   }
-
 
   let hide = () => {
     let tooltip=$("#tooltip");
@@ -70,9 +93,10 @@ let ualTooltipService = function () {
     // template
     var $body = angular.element(document.getElementsByTagName("body"));
     $body.append(template);
-    //console.log($("#tooltip"));
   }
+
   initialize();
+
   return { show, hide };
 };
 export default ualTooltipService;
--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSourceItem/ualDataSourceItem.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSourceItem/ualDataSourceItem.controller.js
@@ -11,12 +11,6 @@ class UalDataSourceItemController {
     let sibling = $("#datasourceItem_" + id);
     let hasEllipsis = (window.isIE) ? ((sibling.outerWidth(true) + 16) >= container.width()) : (sibling.width() > container.width());
 
-    this.tooltipOptions = {
-      left: hasEllipsis ? 2 : -4,
-      right: hasEllipsis ? (window.isIE ? 3 : 4) : (window.isIE ? 10 : 15),
-      top: hasEllipsis ? 2 : (window.isIE ? 2 : 5)
-    }
-
     return (hasEllipsis) ? "datasourceContainer_" + this.datasourceItem.id : "datasourceItem_" + this.datasourceItem.id;
   }
 
@@ -28,8 +22,7 @@ class UalDataSourceItemController {
     this._ualTooltipService.show({
       container:this.datasourceContainer(),
       text:this.datasourceItem.description,
-      adjust:this.tooltipOptions,
-      position:"right"
+      position:"left",
     })
   }
 
