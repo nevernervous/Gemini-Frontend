8a514ce2f4e673b40d5e66bcbb66efb6ee1d5bf5
Iris Ortega <Iris Ortega>
2016-05-11 15:29:09 -0500

    Tooltip work in progress

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
@@ -61,7 +61,7 @@ class UalDataSourceController {
     }
 
     toogleSelected(item) {
-        this.selected = this.isActive(item.id) ? null : item;
+        this.selected = item;
         this.hideTooltip();
     }
 
