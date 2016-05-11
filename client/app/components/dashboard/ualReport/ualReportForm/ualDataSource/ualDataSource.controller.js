ff833048705444df93e7c45f76d34b166b861262
Iris Ortega <Iris Ortega>
2016-05-11 15:30:53 -0500

    Merge changes

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
@@ -2,17 +2,14 @@ import $ from 'jquery';
 
 class UalDataSourceController {
     /*@ngInject*/
-    constructor($rootScope, close, DataSource, selected, ualDataSourceChangeModal, ualDataSourceCancelModal, $filter, $animate, $timeout) {
+    constructor($rootScope, close, DataSource, selected, ualDataSourceChangeModal, ualDataSourceCancelModal, $filter) {
         this._close = close;
         this._datasource = DataSource;
         this._cancelmodal = ualDataSourceCancelModal;
         this._changemodal = ualDataSourceChangeModal;
         this._selected = selected;
         this._filter = $filter;
-        this._timeout = $timeout;
         this.searchTerm = {};
-
-        this._animate = $animate;
 
         this.datasources;
         this.selected = selected;
@@ -21,7 +18,6 @@ class UalDataSourceController {
         this._suscriptions = [];
         this._suscriptions.push($rootScope.$on('SESSION.LOGOUT', () => this._closemodal(true)));
         this._suscriptions.push($rootScope.$on('SESSION.EXPIRED', () => this._closemodal(true)));
-        this._suscriptions.push($rootScope.$on('RENDER.END', () => this.columnCount()));
         this._suscriptions.push($rootScope.$on('$stateChangeSuccess', () => this._closemodal(false)));
     }
 
@@ -65,60 +61,13 @@ class UalDataSourceController {
         this.hideTooltip();
     }
 
-    columnCount() {
-      if(this._columnCountPromise){
-        this._timeout.cancel(this._columnCountPromise);
-      }
-      this._columnCountPromise = this._timeout( () => {
-        let total_items = this._filter("filterBy")(this.datasources.items, this.searchTerm).length;
-        let total_groups = 0;
-
-        // NOTE: REMOVED FOR PERFORMANCE ISSUES
-        // let total_groups = _.reduce(this.datasources.groups, (total, group) => {
-        //   return total += this.shouldShow(group) ? 1 : 0
-        // }, 0 );
-
-        let total = ( total_items + total_groups ) * 36;
-      let $container = angular.element(document.getElementById("content-list"))[0];
-      let rows = Math.ceil(total / $container.clientHeight);
-
-      this.columns = rows > 3 ? "columns-4" : "columns-" + rows;
-        $('content-list').mCustomScrollbar("update");
-      }, 750);
-    }
-
-    // checkOverflow() {
-    //     if (this.finishItemRender) {
-    //         let $marker = angular.element(document.getElementById("overflow-marker"))[0];
-    //         let $container = angular.element(document.getElementById("content-list"))[0];
-    //         let $resizableContainer = angular.element(document.getElementById("resizable-container"))[0];
-    //         let $dataSourceList = angular.element(document.getElementById("data-source-list"))[0];
-
-    //         if (!$marker || !$container) {
-    //             return;
-    //         }
-
-    //         let markerWidth = $marker.offsetLeft;
-    //         let containerWidth = $container.clientWidth;
-
-    //         let hasHorizontalOverflow = markerWidth > containerWidth;
-    //         let needHorizontalFill = $container.clientHeight > $resizableContainer.clientHeight;
-    //         let action = (!(hasHorizontalOverflow || !needHorizontalFill)) ? "addClass" : "removeClass" ;
-
-    //         this._animate[action]($dataSourceList, '-horizontal-fill').then(() => {
-    //                 this.hasLoaded = true;
-    //             });
-
-    //     }
-    // }
-
     hideTooltip(){
       $(".-tooltip").removeClass("-show-tooltip");
       $("[ual-tooltip-show]").prop("ual-tooltip-show", false);
     }
 
     _initialize() {
-      this.columns = "columns-1";
+      // this.columns = "columns-1";
       this._datasource.all('group')
       .then(response => {
         this.datasources = response.data;
