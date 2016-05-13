e66a7f624b88900a4e92478b5eb5786550f84b35
gzepol2 <gerardo.lopez@globant.com>
2016-05-13 15:49:39 -0500

    modify datasourceService for return all variables by data source

--- client/app/services/datasource/datasource.service.js
+++ client/app/services/datasource/datasource.service.js
@@ -21,10 +21,10 @@ let datasourceService = function (Properties, ServicesTransform, $http, $q) {
   let _serialize = (requests, deferred = $q.defer()) => {
 
     let head = _.head(requests);
-    if ( head ) {
-      $http(head).then( response => {
+    if (head) {
+      $http(head).then(response => {
         const tails = _.drop(requests);
-        if ( _.isEmpty(tails) ) {
+        if (_.isEmpty(tails)) {
           deferred.resolve(response);
         } else {
           deferred.notify(response);
@@ -40,52 +40,62 @@ let datasourceService = function (Properties, ServicesTransform, $http, $q) {
 
   let variables = (datasource) => {
     let deferred = $q.defer();
-    let response = {
-      data: { groups: [], items: [] }
-    };
+    let transformation = [ServicesTransform.get('simple')];
 
-
-    groups(datasource)
-    .then(groups => {
-
-      // CREATE REQUEST ARRAY
-      let transformation = [ServicesTransform.get('simple')];
-      let requests = _.map(groups.data, group => {
-        return {
-          method: 'GET',
-          url: `${endpoint}/${datasource.id}/ColumnGroups/${group.groupId}/Columns`,
-          // Force NO cache to fix Bug 22520.
-          cache: false, //Properties.cache,
-          transformResponse: ServicesTransform.generate(transformation)
+    $http.get(`${endpoint}/${datasource.id}/Columns`, {
+      cache: Properties.cache,
+      transformResponse: ServicesTransform.generate(transformation)
+    }).then(response => {
+      let variables = {
+        data: {
+          items: response.data
         }
-      });
+      }
+      deferred.resolve(variables);
+    }
+    );
 
-      // FIRST NOTIFICATION WITH EMPTY GROUPS
-      response.data.groups = _.map(groups.data, group => {
-        return { data: group }
-      });
-      deferred.notify(response);
+    // groups(datasource)
+    //   .then(groups => {
 
-      // SERIALIZATION REQUEST
-      _serialize(requests)
-      .then(
-        done => { // RESOLVE PROMISE WITH LAST NOTIFICATION
-          if (done) {
-            let group = _.find(response.data.groups, group => group.data.groupId === done.data[0].group.groupId);
-            if (group) group.items = done.data;
-            response.data.items = _.union(response.data.items, done.data);
-          }
-          deferred.resolve(response);
-        },
-        error => deferred.reject(error),
-        progress => { // NOTIFICATION FOR EACH GROUP VARIABLES
-          let group = _.find(response.data.groups, group => group.data.groupId === progress.data[0].group.groupId);
-          if (group) group.items = progress.data;
-          response.data.items = _.union(response.data.items, progress.data);
-          deferred.notify(response);
-        }
-      )
-    });
+    //     // CREATE REQUEST ARRAY
+    //     let transformation = [ServicesTransform.get('simple')];
+    //     let requests = _.map(groups.data, group => {
+    //       return {
+    //         method: 'GET',
+    //         url: `${endpoint}/${datasource.id}/ColumnGroups/${group.groupId}/Columns`,
+    //         // Force NO cache to fix Bug 22520.
+    //         cache: false, //Properties.cache,
+    //         transformResponse: ServicesTransform.generate(transformation)
+    //       }
+    //     });
+
+    //     // FIRST NOTIFICATION WITH EMPTY GROUPS
+    //     response.data.groups = _.map(groups.data, group => {
+    //       return { data: group }
+    //     });
+    //     deferred.notify(response);
+
+    //     // SERIALIZATION REQUEST
+    //     _serialize(requests)
+    //       .then(
+    //       done => { // RESOLVE PROMISE WITH LAST NOTIFICATION
+    //         if (done) {
+    //           let group = _.find(response.data.groups, group => group.data.groupId === done.data[0].group.groupId);
+    //           if (group) group.items = done.data;
+    //           response.data.items = _.union(response.data.items, done.data);
+    //         }
+    //         deferred.resolve(response);
+    //       },
+    //       error => deferred.reject(error),
+    //       progress => { // NOTIFICATION FOR EACH GROUP VARIABLES
+    //         let group = _.find(response.data.groups, group => group.data.groupId === progress.data[0].group.groupId);
+    //         if (group) group.items = progress.data;
+    //         response.data.items = _.union(response.data.items, progress.data);
+    //         deferred.notify(response);
+    //       }
+    //       )
+    //   });
 
     return deferred.promise;
   }
