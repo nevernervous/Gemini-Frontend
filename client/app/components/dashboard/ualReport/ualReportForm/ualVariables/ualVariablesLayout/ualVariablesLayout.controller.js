92d65ea4f036826ce6ecb092f025db071508ebdb
gzepol2 <gerardo.lopez@globant.com>
2016-05-13 13:08:22 -0500

    add tooltip on buttons for add variables

--- client/app/components/dashboard/ualReport/ualReportForm/ualVariables/ualVariablesLayout/ualVariablesLayout.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualVariables/ualVariablesLayout/ualVariablesLayout.controller.js
@@ -1,7 +1,20 @@
 class UalVariablesLayoutController {
   /*@ngInject*/
-  constructor() {
+  constructor(ualTooltipService) {
     this.name = 'ualVariablesLayout';
+    this._ualTooltipService = ualTooltipService;
+  }
+
+  showTooltip(container, description, position = 'top') {
+    this._ualTooltipService.show({
+      container: container,
+      text: description,
+      position: position
+    });
+  }
+
+  hideTooltip() {
+    this._ualTooltipService.hide();
   }
 }
 
--- client/app/components/dashboard/ualReport/ualReportForm/ualVariables/ualVariablesLayout/ualVariablesLayout.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualVariables/ualVariablesLayout/ualVariablesLayout.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import ualVariablesLayoutComponent from './ualVariablesLayout.component';
+import ualTooltipService from '../../../../../../common/ualTooltip/ualTooltip.service';
 
 let ualVariablesLayoutModule = angular.module('ualVariablesLayout', [])
 
-.component('ualVariablesLayout', ualVariablesLayoutComponent);
+.component('ualVariablesLayout', ualVariablesLayoutComponent)
+.factory('ualTooltipService',ualTooltipService);
 
 export default ualVariablesLayoutModule;
