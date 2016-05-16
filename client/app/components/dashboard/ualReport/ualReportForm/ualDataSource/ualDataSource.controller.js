9f61318cf0af0a2cdb025efd17436d1a11c16ea8
Victor Vazquez <needlinkspeed@hotmail.com>
2016-05-16 16:49:51 -0500

    Selected item added with report object

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
@@ -1,40 +1,36 @@
 class UalDataSourceController {
-    /*@ngInject*/
-    constructor($rootScope, DataSource,  ualDataSourceChangeModal, ualDataSourceCancelModal, ualTooltipService) {
-        this._datasource = DataSource;
-        this._cancelmodal = ualDataSourceCancelModal;
-        this._changemodal = ualDataSourceChangeModal;
-        this._ualTooltipService = ualTooltipService;
+  /*@ngInject*/
+  constructor($rootScope, DataSource, ualDataSourceChangeModal, ualDataSourceCancelModal, ualTooltipService) {
+    this._datasource = DataSource;
+    this._cancelmodal = ualDataSourceCancelModal;
+    this._changemodal = ualDataSourceChangeModal;
+    this._ualTooltipService = ualTooltipService;
 
-        this.searchTerm = {};
+    this.searchTerm = {};
 
-        this.datasources;
+    this.datasources;
 
-    }
+  }
 
-    hasChange() {
-        return (!this.selected && this.selected) || (this.selected && this.selected && (this.selected.id !== this.selected.id));
-    }
+  isActive(itemId) {
+    return (!!this.selected.get()) && this.selected.get().id === itemId;
+  }
 
-    isActive(itemId) {
-        return (this.selected) && this.selected.id === itemId;
-    }
+  toogleSelected(item) {
+    this.selected.set(item);
+    this.hideTooltip();
+  }
 
-    toogleSelected(item) {
-        this.selected = item;
-        this.hideTooltip();
-    }
+  hideTooltip() {
+    this._ualTooltipService.hide();
+  }
 
-    hideTooltip(){
-      this._ualTooltipService.hide();
-    }
-
-    $onInit() {
-      this._datasource.all('group')
+  $onInit() {
+    this._datasource.all('group')
       .then(response => {
         this.datasources = response.data;
       });
-    }
+  }
 }
 
 export default UalDataSourceController;
