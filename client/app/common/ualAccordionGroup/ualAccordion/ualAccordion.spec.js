87ee7069d84095eec1e13c9727b45a7944c4a499
Jeronimo Vallelunga <jeronimo.vallelunga@gmail.com>
2016-05-18 18:20:58 +0000

    [WIP] cleaning specs

--- client/app/common/ualAccordionGroup/ualAccordion/ualAccordion.spec.js
+++ client/app/common/ualAccordionGroup/ualAccordion/ualAccordion.spec.js
@@ -1,6 +1,6 @@
 import UalAccordionModule from './ualAccordion'
 import UalAccordionController from './ualAccordion.controller';
-import UalAccordionComponent from './ualAccordion.component';
+import UalAccordionDirective from './ualAccordion.directive';
 import UalAccordionTemplate from './ualAccordion.html';
 
 describe('UalAccordion', () => {
@@ -14,13 +14,6 @@ describe('UalAccordion', () => {
     };
   }));
 
-  describe('Module', () => {
-    // top-level specs: i.e., routes, injection, naming
-    it('has a name property [REMOVE]', () => {
-      expect(UalAccordionModule).to.have.property('name');
-    });          
-  });
-
   describe('Controller', () => {
     // controller specs
     it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
@@ -29,29 +22,4 @@ describe('UalAccordion', () => {
     });
   });
 
-  describe('Template', () => {
-    // template specs
-    // tip: use regex to ensure correct bindings are used e.g., {{  }}
-    const template = $('<ual-accordion/>').html(UalAccordionTemplate);
-    it('has at least one element [REMOVE]', () => {
-      expect(template.children()).to.have.length.of.at.least(1);
-    });
-  });
-
-  describe('Component', () => {
-      // component/directive specs
-      let component = UalAccordionComponent;
-
-      it('includes the intended template',() => {
-        expect(component.template).to.equal(UalAccordionTemplate);
-      });
-
-      it('uses `controllerAs` syntax', () => {
-        expect(component).to.have.property('controllerAs');
-      });
-
-      it('invokes the right controller', () => {
-        expect(component.controller).to.equal(UalAccordionController);
-      });
-  });
 });
