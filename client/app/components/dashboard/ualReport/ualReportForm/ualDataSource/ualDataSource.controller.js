f8ac712183eb91bb8bc74594d22ef0eeaae0f73e
Victor Vazquez <v.vazquez@globant.com>
2016-05-27 09:25:56 -0500

    Commit for dev

--- client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualDataSource/ualDataSource.controller.js
@@ -1,15 +1,18 @@
 class UalDataSourceController {
   /*@ngInject*/
-  constructor($rootScope,ualDataSourceChangeModal, DataSource, ualDataSourceCancelModal, ualTooltipService) {
+  constructor($rootScope, ualDataSourceChangeModal, DataSource, ualDataSourceCancelModal, ualTooltipService, $timeout) {
     this._datasource = DataSource;
     this._cancelmodal = ualDataSourceCancelModal;
     this._changemodal = ualDataSourceChangeModal;
     this._ualTooltipService = ualTooltipService;
+    this._timeout = $timeout;
 
-    this.searchTerm = {};
+    this.searchTerm;
     this.datasources;
     this.rootScope = $rootScope;
     this.selected;
+
+   this.groupsTotals = [];
   }
 
 
@@ -17,14 +20,28 @@ class UalDataSourceController {
     return !!this.selected.get() && this.selected.get().id === itemId;
   }
 
-  isFirstInGroup(index){
+  isFirstInGroup(index) {
     let result = false;
-    var previousItem = index >= 1 ? this.datasources[index -1] : undefined;
+    var previousItem = index >= 1 ? this.datasources[index - 1] : undefined;
     var currentItem = this.datasources[index];
-    if(!previousItem || previousItem.group.groupId != currentItem.group.groupId){
+    if (!previousItem || previousItem.group.groupId != currentItem.group.groupId) {
       result = true;
     }
     return result;
+  }
+
+  filterData() {
+    this._timeout(() => {
+      this.total = 0;
+      const filtered = _.forEach(this.datasources, (item) => {
+        let noFilter = !this.searchTerm || _.isEmpty(this.searchTerm);
+        let hasMatch = (!!this.searchTerm && !!item) && item.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+        item.show = noFilter || hasMatch;
+        this.total  += item.show ? 1 : 0;
+        this.groupsTotals[item.group.groupId] = (this.groupsTotals[item.group.groupId] || 0) + 1 ;
+      });
+      return filtered;
+    }, 0);
   }
 
 
@@ -63,6 +80,7 @@ class UalDataSourceController {
     this._datasource.all('group')
       .then(response => {
         this.datasources = response.data;
+        this.filterData();
       });
   }
 }
