a5aa7b4cfb210fcea413e648ae09f7f9f2fd2d50
salvador.elvira <salvador_elvira@outlook.com>
2016-06-06 18:35:07 -0500

    Act to FW: Fixing select all and heights

--- client/app/components/dashboard/ualReport/ualReportForm/ualVariablesLayout/ualVariablesMultiSelect/ualVariablesMultiSelect.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualVariablesLayout/ualVariablesMultiSelect/ualVariablesMultiSelect.controller.js
@@ -30,8 +30,10 @@ class UalVariablesMultiSelectController {
   }
 
   selectAll() {
-    this.selectedReference = _.filter(this.avaiableVariables, (variable)=>{return variable._visible;});
-    _.each(this.selectedReference, (item) => item.selected = true);
+    _.each(this.avaiableVariables, (item,idx) => {this.avaiableVariables[idx].selected = false;});
+    let visible = _.filter(this.avaiableVariables, (variable)=>{return variable._visible;});
+    console.log(visible.length);
+    _.each(visible, (item) => item.selected = true);
   }
 
 
