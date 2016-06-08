daee512f2abe2288bccf58bf6fde66e1e7fc4d7a
nevernervous <brndncostoya@gmail.com>
2016-06-08 09:41:25 -0500

    Added messages

--- client/app/components/dashboard/ualReport/ualReportForm/ualFilters/ualFilterCondition/ualFilterCondition.controller.js
+++ client/app/components/dashboard/ualReport/ualReportForm/ualFilters/ualFilterCondition/ualFilterCondition.controller.js
@@ -4,14 +4,27 @@ class UalFilterConditionController {
     this.name = 'ualFilterCondition';
     this.valueVariable = [
       {
-        value:false,
+        value: false,
         text: "Value"
       },
       {
-        value:true,
+        value: true,
         text: "Variable"
       }
-      ];
+    ];
+  }
+
+  isValid() {
+    let response = {
+      ReturnObject: { "Id": 1, "Value": "Input Value", "Type": "String" },
+      HasError: false,
+      Errors: [""]
+    };
+    return response.HasError;
+  }
+
+  getVariableName() {
+    return !!this.condition && !!this.condition.selectedVariable ? this.condition.selectedVariable.name : "[variable name]";
   }
 
 }
