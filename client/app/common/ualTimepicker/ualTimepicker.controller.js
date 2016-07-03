b5486b24c898005239a86d0ac7ca4c6641adb516
Jeronimo Vallelunga <jeronimo.vallelunga@gmail.com>
2016-07-03 19:03:29 +0000

    [wip] timepicker IE support
    Former-commit-id: 9500240822134c913f445d2ae09593f5f32e9a78

--- client/app/common/ualTimepicker/ualTimepicker.controller.js
+++ client/app/common/ualTimepicker/ualTimepicker.controller.js
@@ -1,8 +1,10 @@
 class UalTimepickerController {
   /*@ngInject*/
-  constructor($element) {
+  constructor($element, $scope, $timeout) {
     this.name = 'ualTimepicker';
     this.$element = $element;
+    this.$scope = $scope;
+    this.$timeout = $timeout;
 
     this.dom = {
       hours: null,
@@ -10,35 +12,37 @@ class UalTimepickerController {
       clock: null
     }
 
-    this.reset = true;
-
     let _hour = null;
     let _minute = null;
     this.time = {
-      hour: (newHour) => {
+      hour: (newHour, arrow) => {
         // Note that newHour can be undefined for two reasons:
         // 1. Because it is called as a getter and thus called with no arguments
         // 2. Because the property should actually be set to undefined. This happens e.g. if the
         //    input is invalid
         if ( arguments.length && /^[0-9]+$/.test(newHour) ) {
           newHour = newHour < 0 ? 23 : parseInt(newHour) % 24;
-          newHour = newHour > 9 ? newHour : `0${newHour}`;
+          _hour = newHour > 9 ? newHour : `0${newHour}`;
 
-          return (_hour = newHour);
+          if ( newHour > 2 && !arrow ) {
+            this.dom.minutes.focus();
+          }
         }
 
         return _hour;
       },
-      minute: (newMinute) => {
+      minute: (newMinute, arrow) => {
         // Note that newHour can be undefined for two reasons:
         // 1. Because it is called as a getter and thus called with no arguments
         // 2. Because the property should actually be set to undefined. This happens e.g. if the
         //    input is invalid
         if ( arguments.length && /^[0-9]+$/.test(newMinute) ) {
           newMinute = newMinute < 0 ? 59 : parseInt(newMinute) % 60;
-          newMinute = newMinute > 9 ? newMinute : `0${newMinute}`
+          _minute = newMinute > 9 ? newMinute : `0${newMinute}`;
 
-          return (_minute = newMinute);
+          if ( newMinute > 5 && !arrow ) {
+            this.dom.clock.focus();
+          }
         }
         return _minute;
       }
@@ -47,91 +51,70 @@ class UalTimepickerController {
     this.actions = {}
 
     this.actions.hours = {
-      'ArrowUp': (e) => {
+      '38': (e) => { // UP
         e.preventDefault();
         const value = parseInt(_hour || 0) + 1;
-        this.time.hour(value);
+        this.time.hour(value, true);
       },
-      'ArrowDown': (e) => {
+      '40': (e) => { // DOWN
         e.preventDefault();
         const value = parseInt(_hour || 0) - 1;
-        this.time.hour(value < 0 ? 23 : value);
+        this.time.hour(value < 0 ? 23 : value, true);
       },
-      'ArrowLeft': (e) => {
+      '37': (e) => { // LEFT
         e.preventDefault();
       },
-      'ArrowRight': (e) => {
+      '39': (e) => { // RIGHT
         e.preventDefault();
         this.dom.minutes.focus();
       }
     }
 
     this.actions.minutes = {
-      'ArrowUp': (e) => {
+      '38': (e) => { // UP
         e.preventDefault();
-        this.time.minute(parseInt(_minute || 0) + 1);
+        const value = parseInt(_minute || 0) + 1;
+        this.time.minute(value, true);
       },
-      'ArrowDown': (e) => {
+      '40': (e) => { // DOWN
         e.preventDefault();
         const value = parseInt(_minute || 0) - 1;
-        this.time.minute(value < 0 ? 59 : value);
+        this.time.minute(value < 0 ? 59 : value, true);
       },
-      'ArrowLeft': (e) => {
+      '37': (e) => { // LEFT
         e.preventDefault();
         this.dom.hours.focus();
       },
-      'ArrowRight': (e) => {
+      '39': (e) => { // RIGHT
         e.preventDefault();
         this.dom.clock.focus();
       }
     }
 
     this.actions.clock = {
-      'ArrowLeft': (e) => {
+      '37': (e) => {
         e.preventDefault();
         this.dom.minutes.focus();
       }
     }
   }
 
-  hoursdown(e) {
-    if ( this.reset && _.startsWith(e.code, 'Digit') ) {
-      e.preventDefault();
-      this.reset = false;
-      this.time.hour(e.code[e.code.length - 1]);
-    } else {
-      const action = this.actions.hours[e.code];
-      action ? action(e) : _.noop();
-    }
+  hoursdown(event) {
+    const action = this.actions.hours[event.which];
+    action ?
+      this.$scope.$apply(() => action(event)):
+      _.noop();
   }
 
-  hoursup(e) {
-    if ( _.startsWith(e.code, 'Digit') &&
-        parseInt(e.target.value) > 2) {
-      this.dom.minutes.focus();
-    }
-  }
-
-  minutesdown(e) {
-    if ( this.reset && _.startsWith(e.code, 'Digit') ) {
-      e.preventDefault();
-      this.reset = false;
-      this.time.minute(e.code[e.code.length - 1]);
-    } else {
-      const action = this.actions.minutes[e.code];
-      action ? action(e) : _.noop();
-    }
-  }
-
-  minutesup(e) {
-    if ( _.startsWith(e.code, 'Digit') &&
-        parseInt(e.target.value) > 5) {
-      this.dom.clock.focus();
-    }
+  minutesdown(event) {
+    const action = this.actions.minutes[event.which];
+    action ?
+      this.$scope.$apply(() => action(event)):
+      _.noop();
   }
 
   clockdown(e) {
-    const action = this.actions.clock[e.code];
+    const action = this.actions.clock[e.which];
     action ? action(e) : _.noop();
   }
 
@@ -139,6 +122,17 @@ class UalTimepickerController {
     this.dom.hours   = $(this.$element).find('.hours');
     this.dom.minutes = $(this.$element).find('.minutes');
     this.dom.clock   = $(this.$element).find('.clock');
+
+    // ACTIONS
+    this.dom.hours.on('keydown', event => this.hoursdown(event) );
+    this.dom.hours.on("click", function () { $(this).select(); });
+    this.dom.hours.on("focus", function () { $(this).select(); });
+
+    this.dom.minutes.on('keydown', event => this.minutesdown(event) );
+    this.dom.minutes.on("click", function () { $(this).select(); });
+    this.dom.minutes.on("focus", function () { $(this).select(); });
+
+    this.dom.clock.on('keydown', event => this.clockdown(event) );
   }
 }
 
