584ccfce9d9cf89211a2e9f154377e645320e533
nevernervous <brndncostoya@gmail.com>
2016-07-05 14:36:36 -0500

    Oshioshi
    Former-commit-id: 4398c9acb3877622276dfa940b1583dfac11d327

--- client/app/validators/ualDateValidator/ualDateValidator.directive.js
+++ client/app/validators/ualDateValidator/ualDateValidator.directive.js
@@ -13,14 +13,36 @@ class ualDateValidatorDirective {
   }
 
   link($scope, elem, attr, ctrl) {
-    ctrl.$validators.dateValue = function (modelValue, viewValue) {
-      if(!!viewValue && attr.datatype === "Date"){
-        let dateArray=viewValue.split("/");
+    ctrl.$validators.dateValue = function (viewValue) {
+      if (!!viewValue && attr.datatype === "Date") {
+        let dateArray = viewValue.split("/");
         let tempDate = new Date(dateArray[2], --dateArray[0], dateArray[1]);
         return dateArray[0] === tempDate.getMonth();
       }
       return true;
     };
+
+    let validSecondValue = (viewValue) => {
+      let validity = true;
+      if (!!viewValue && attr.datatype === "Date" && attr.secondDate) {
+        let firstDate = viewValue,
+          secondDate = attr.secondDate;
+        let firstDateArray = firstDate.split("/");
+        let secondDateArray = secondDate.split("/");
+        let tempFirstDate = new Date(firstDateArray[2], --firstDateArray[0], firstDateArray[1]);
+        let tempSecondDate = new Date(secondDateArray[2], --secondDateArray[0], secondDateArray[1]);
+        validity = tempSecondDate > tempFirstDate;
+      }
+      ctrl.$setValidity('secondDate', validity);
+      return viewValue;
+    };
+
+    ctrl.$parsers.unshift(validSecondValue);
+    ctrl.$formatters.push(validSecondValue);
+
+    attr.$observe("secondDate", function () {
+      return validSecondValue(ctrl.$viewValue);
+    });
   }
 }
 
